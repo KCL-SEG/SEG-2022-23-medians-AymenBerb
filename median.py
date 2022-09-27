734139e3eced82bc4b163d344354d3611041065a
Aymen Berbache <aymenberbache21@gmail.com>
2022-09-27 13:26:10 +0100

    Exercise done

--- median.py
+++ median.py
@@ -1,6 +1,9 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from operator import index
+
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -10,3 +13,18 @@ while True:
     else:
         break
 print(numbers)
+
+def getMedian(listOfNumbers):
+    result = 0
+    listOfNumbers.sort()
+    if (len(listOfNumbers) % 2 == 1):
+        # If the number of elements inside the list is odd, we take the element at the exact middle of the list.
+        result = listOfNumbers[len(listOfNumbers) // 2]
+    else:
+        indexOfFirstMiddleNumber = int((len(listOfNumbers) / 2) - 1)
+        #If the number of element inside the list is even, we perform the average of the two middle numbers.
+        result = (listOfNumbers[indexOfFirstMiddleNumber] + listOfNumbers[indexOfFirstMiddleNumber + 1]) / 2
+    return result
+
+print(getMedian(numbers))
+
